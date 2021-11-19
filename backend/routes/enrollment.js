bc400b82c131c8f7f129452eb79d276c5bacc0bc
xJamesScott <50638563+xJamesScott@users.noreply.github.com>
2021-11-18 21:07:59 -0500

    added csv processing

---
+++ backend/routes/enrollment.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var Papa = require('papaparse');
+var zip = require('express-zip');
+
+router.get('/', function (req, res) {
+    // destructure csv
+    const { body: { csv }, query } = req;
+
+    // csv file and path
+    const fileName = 'enrollment.csv';
+    const path = `./enrollment/${fileName}`
+
+    // create enrollment directory
+    fs.mkdir("./enrollment", { recursive: true }, (err) => {
+        if (err) {
+            console.log("ERROR making directory: " + err)
+        }
+    });
+
+    // parse csv
+    let filePaths = []
+    const processData = (enrollmentUpload) => {
+
+        const sortedUsers = enrollmentUpload.sort(function (a, b) {
+            let nameA = a.name
+            let nameB = b.name
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+            return 0;
+        });
+
+        // // 2 - Duplicate users
+
+        let usersNoDups = {};
+        let noDupsArr = Object.values(usersNoDups);
+
+        sortedUsers.forEach((user, i) => {
+            if (!usersNoDups[0]) {
+                usersNoDups[user["User Id"]] = user;
+            }
+
+            if (usersNoDups[0] && sortedUsers[i]) {
+                for (let j = 0; j < noDupsArr.length; j++) {
+                    if (sortedUsers[i]["User Id"] == noDupsArr[j]["User Id"]) {
+                        if (sortedUsers[i]["Version"] > noDupsArr[j]["Version"]) {
+                            noDupsArr.splice(j, 1);
+                            usersNoDups[user]["User Id"] = user;
+                        }
+                    } else {
+                        usersNoDups[user]["User Id"] = user;
+                    }
+                }
+            }
+        });
+
+        // Seperate by insurance company
+        let usersByCompany = {}
+        noDupsArr = Object.values(usersNoDups);
+
+        for (let h = 0; h < noDupsArr.length; h++) {
+            usersByCompany[noDupsArr[h]["Insurance Company"]] = noDupsArr[h];
+        }
+
+        // create & save file for each company
+        Object.values(usersByCompany).forEach((companyUsers) => {
+            // let filePaths = []
+            const csvProcessed = Papa.unparse(companyUsers, { header: true });
+            const fileName = `${companyUsers["Insurance Company"]}.csv`;
+            const companyCsvPath = `./enrollment/${fileName}`;
+            console.log("Writing file...")
+            fs.writeFile(companyCsvPath, csvProcessed, err => {
+                if (err) {
+                    console.error("ERROR writing csv: " + err)
+                    return
+                }
+            })
+            filePaths = [...filePaths,
+            {
+                path: companyCsvPath,
+                name: fileName
+            }]
+        });
+        console.log(filePaths)
+    };
+
+    const createParsedCSV = (err, csv) => {
+        Papa.parse(csv, { // csv parser
+            header: true,
+            complete: (res) => processData(res.data) // parser callback
+        })
+    };
+
+    fs.readFile(path, "utf8", (err, csv) => createParsedCSV(err, csv))
+
+
+    res.send('All Files processed!');
+});
+
+module.exports = router;
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>
+        To process enrollment CSVs please go to:
+        <br />
+        <br />
+        Go to <a href="http://localhost:8000/enrollment">http://localhost:8000/enrollment</a>
+      </h3>
     </div>
   );
 }
